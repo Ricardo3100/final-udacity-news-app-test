fc26c449131e839e729c5e831e5abc7a142b5e5e
Ricardo3100 <38477282+Ricardo3100@users.noreply.github.com>
2019-03-11 20:47:35 -0700

    updating the string value

--- app/src/main/java/com/example/ricardo/grabbingjsonattributestest/SettingsActivity.java
+++ app/src/main/java/com/example/ricardo/grabbingjsonattributestest/SettingsActivity.java
@@ -13,6 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.ricardo.grabbingjsonattributestest;
 
 import android.content.SharedPreferences;
@@ -38,10 +53,9 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
-
-
-            Preference Technology = findPreference(getString(R.string.settings_order_by_key));
-            bindPreferenceSummaryToValue(Technology);
+//            the newest story preference will populate the newest stories
+            Preference StoryList = findPreference(getString(R.string.settings_page_size_key));
+            bindPreferenceSummaryToValue(StoryList);
 
         }
 
